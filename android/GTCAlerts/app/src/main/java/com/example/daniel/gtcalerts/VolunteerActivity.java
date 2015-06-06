44ad295e111efd03030fad0849668ea420c08f75
Daniel Nelson <daniel.nelson@gmail.com>
2015-06-06 17:16:00 -0400

    Added switch

--- android/GTCAlerts/app/src/main/java/com/example/daniel/gtcalerts/VolunteerActivity.java
+++ android/GTCAlerts/app/src/main/java/com/example/daniel/gtcalerts/VolunteerActivity.java
@@ -2,17 +2,8 @@ package com.example.daniel.gtcalerts;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.content.Intent;
 import android.widget.ListView;
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 
 /**
@@ -36,27 +27,43 @@ public class VolunteerActivity extends Activity {
 
         mainListView = (ListView) findViewById( R.id.mainListView );
 
-        // Create and populate a List of planet names.
-        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
-                "Jupiter", "Saturn", "Uranus", "Neptune"};
-        ArrayList<String> planetList = new ArrayList<String>();
-        planetList.addAll( Arrays.asList(planets) );
+
+        //ArrayList<String> planetList = new ArrayList<String>();
+       // planetList.addAll( Arrays.asList(planets) );
 
         // Create ArrayAdapter using the planet list.
-        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow);
+
+        //foreach
 
         // Add more planets. If you passed a String[] instead of a List<String>
         // into the ArrayAdapter constructor, you must not add more items.
         // Otherwise an exception will occur.
-        listAdapter.add( "Ceres" );
-        listAdapter.add( "Pluto" );
-        listAdapter.add( "Haumea" );
-        listAdapter.add( "Makemake" );
-        listAdapter.add( "Eris" );
+        listAdapter.add( "ABC Event 1" );
+        listAdapter.add( "ABC Event 2" );
+        listAdapter.add( "ABC Event 3" );
+        listAdapter.add( "ABC Event 4" );
+        listAdapter.add( "ABC Event 5" );
+/*
+        listAdapter.setOnItemClickListener(new OnItemClickListener() {
+
+
+
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                Log.v("clicked", (String) ((TextView) arg1).getText());
+            }
+
+        });
+        */
 
         // Set the ArrayAdapter as the ListView's adapter.
         mainListView.setAdapter( listAdapter );
     }
+
+
+
+
 
 
 }
