e3fd506670b34a74079b0cb90015aa4c737cc7a1
Lou Franco <lfranco@greenwave-solutions.com>
2015-06-06 12:43:17 -0400

    Initial screen and app icon

---
+++ ios/GtC Alerts/GtC Alerts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GtC Alerts
+//
+//  Created by Louis Franco on 6/6/15.
+//  Copyright (c) 2015 Gardening the Community. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
