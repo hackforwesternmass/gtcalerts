733a3020f0446608070fc0a2d50dea94602ba0fd
Lou Franco <lfranco@greenwave-solutions.com>
2015-06-06 14:28:56 -0400

    Notification view with switch and table

---
+++ ios/GtC Alerts/GtC Alerts/NotificationViewController.m
@@ -0,0 +1,55 @@
+//
+//  NotificationViewController.m
+//  GtC Alerts
+//
+//  Created by Louis Franco on 6/6/15.
+//  Copyright (c) 2015 Gardening the Community. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+#import "NotificationTableViewCell.h"
+
+@interface NotificationViewController ()
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.allowPushLabel.text = self.pushLabelText;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NotificationTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"NotificationCell"];
+    cell.dateLabel.text = @"July 14";
+    cell.titleLabel.text = @"Impact Day!";
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
