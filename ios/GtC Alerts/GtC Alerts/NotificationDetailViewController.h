1bb80cd4c55ab3a7d57664b7c01483097e8bc03d
Lou Franco <lfranco@greenwave-solutions.com>
2015-06-06 15:01:16 -0400

    Detail page working

---
+++ ios/GtC Alerts/GtC Alerts/NotificationDetailViewController.h
@@ -0,0 +1,19 @@
+//
+//  NotificationDetailViewController.h
+//  GtC Alerts
+//
+//  Created by Louis Franco on 6/6/15.
+//  Copyright (c) 2015 Gardening the Community. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationDetailViewController : UIViewController
+
+@property (nonatomic, strong) NSString *titleText;
+@property (nonatomic, strong) NSString *detailsText;
+
+@property (nonatomic, strong) IBOutlet UILabel *titleLabel;
+@property (nonatomic, strong) IBOutlet UITextView *detailsTextView;
+
+@end
---
+++ ios/GtC Alerts/GtC Alerts/NotificationDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  NotificationDetailViewController.m
+//  GtC Alerts
+//
+//  Created by Louis Franco on 6/6/15.
+//  Copyright (c) 2015 Gardening the Community. All rights reserved.
+//
+
+#import "NotificationDetailViewController.h"
+
+@interface NotificationDetailViewController ()
+
+@end
+
+@implementation NotificationDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.titleLabel.text = self.titleText;
+    self.detailsTextView.text = self.detailsText;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
